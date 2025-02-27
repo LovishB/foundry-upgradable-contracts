ebb6407a3c4b9993d05f4a348c458c7a4ba92349
Lovish Badlani <42366382+LovishB@users.noreply.github.com>
2025-02-27 20:31:50 +0530

    Update DemoSummation.s.sol

--- script/DemoSummation.s.sol
+++ script/DemoSummation.s.sol
@@ -3,25 +3,26 @@ pragma solidity ^0.8.13;
 
 import {Script, console} from "forge-std/Script.sol";
 import {SummationProxy} from "../src/SummationProxy.sol";
+import {SummationProxyEIP1967} from "../src/SummationProxyEIP1967.sol";
 import {Summation} from "../src/Summation.sol";
 
 contract DemoSummation is Script {
     SummationProxy public proxy;
+    SummationProxyEIP1967 public proxyEIP1967;
     Summation public implementation;
 
     function setUp() public {}
 
     function run() public {
         vm.startBroadcast();
+        uint256 a = 10;
+        uint256 b = 20;
+
         // Deploy implementation contract
         implementation = new Summation();
 
         // Deploy proxy contract with implementation address
         proxy = new SummationProxy(address(implementation));
-
-        uint256 a = 10;
-        uint256 b = 20;
-
         proxy.sum(a, b);
 
         console.log("Implemetation Contract variables");
@@ -33,6 +34,19 @@ contract DemoSummation is Script {
         console.log(proxy.a());
         console.log(proxy.b());
         console.log(proxy.result());
+
+        console.log("Summation using proxy EIP1967");
+        proxyEIP1967 = new SummationProxyEIP1967(address(implementation));
+        (bool success, ) = address(proxyEIP1967).call(
+            abi.encodeWithSignature("sum(uint256,uint256)", a, b)
+        );
+        require(success, "Proxy call failed");
+
+        console.log("ProxyEIP1967 Contract variables");
+        // console.log(success);
+        console.log(proxyEIP1967.a());
+        console.log(proxyEIP1967.b());
+        console.log(proxyEIP1967.result());
 
         vm.stopBroadcast();
     }
